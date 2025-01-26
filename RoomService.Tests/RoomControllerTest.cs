70cc0176d5517c1b7d9fbbc94dbaf921d681df9b
knutekje <knutekjersem@gmail.com>
2025-01-26 15:46:00 +0100

    yesyes

---
+++ RoomService.Tests/RoomControllerTest.cs
@@ -0,0 +1,111 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RoomService.Controllers;
+using RoomService.Models;
+using RoomService.Services;
+using Xunit;
+using FluentAssertions;
+
+
+
+namespace RoomService.Tests;
+
+public class RoomsControllerTests
+{
+    private readonly Mock<IRoomService> _mockRoomService;
+    private readonly RoomsController _controller;
+
+    public RoomsControllerTests()
+    {
+        _mockRoomService = new Mock<IRoomService>();
+        _controller = new RoomsController(_mockRoomService.Object);
+    }
+
+    [Fact]
+    public async Task GetAllRooms_ShouldReturnOkWithRooms()
+    {
+        var rooms = new List<Room>
+        {
+            new Room { Id = 1, RoomNumber = "101", RoomType = "Single", Status = RoomStatus.Clean },
+            new Room { Id = 2, RoomNumber = "102", RoomType = "Double", Status = RoomStatus.Dirty }
+        };
+
+        _mockRoomService.Setup(service => service.GetAllRoomsAsync()).ReturnsAsync(rooms);
+
+        var result = await _controller.GetAllRooms();
+
+        result.Should().BeOfType<OkObjectResult>();
+        var okResult = result as OkObjectResult;
+        okResult!.Value.Should().BeEquivalentTo(rooms);
+    }
+
+    [Fact]
+    public async Task GetRoomById_ShouldReturnNotFound_WhenRoomDoesNotExist()
+    {
+        _mockRoomService.Setup(service => service.GetRoomByIdAsync(It.IsAny<int>())).ReturnsAsync((Room?)null);
+
+        var result = await _controller.GetRoomById(1);
+
+        result.Should().BeOfType<NotFoundObjectResult>();
+        var notFoundResult = result as NotFoundObjectResult;
+        notFoundResult!.Value.Should().BeEquivalentTo(new { Message = "Room with ID 1 not found." });
+    }
+
+    [Fact]
+    public async Task AddRoom_ShouldReturnCreatedRoom()
+    {
+        var newRoom = new Room { RoomNumber = "101", RoomType = "Single", Status = RoomStatus.Clean };
+        var createdRoom = new Room { Id = 1, RoomNumber = "101", RoomType = "Single", Status = RoomStatus.Clean };
+
+        _mockRoomService.Setup(service => service.AddRoomAsync(It.IsAny<Room>())).ReturnsAsync(createdRoom);
+
+        var result = await _controller.AddRoom(newRoom);
+
+        result.Should().BeOfType<CreatedAtActionResult>();
+        var createdResult = result as CreatedAtActionResult;
+        createdResult!.Value.Should().BeEquivalentTo(createdRoom);
+    }
+
+    [Fact]
+    public async Task UpdateRoom_ShouldReturnNotFound_WhenRoomDoesNotExist()
+    {
+        var updatedRoom = new Room { RoomNumber = "101A", RoomType = "Single", Status = RoomStatus.Clean };
+
+        _mockRoomService.Setup(service => service.UpdateRoomAsync(It.IsAny<int>(), It.IsAny<Room>()))
+            .ReturnsAsync((Room?)null);
+
+        var result = await _controller.UpdateRoom(1, updatedRoom);
+
+        result.Should().BeOfType<NotFoundObjectResult>();
+        var notFoundResult = result as NotFoundObjectResult;
+        notFoundResult!.Value.Should().BeEquivalentTo(new { Message = "Room with ID 1 not found." });
+    }
+
+    [Fact]
+    public async Task DeleteRoom_ShouldReturnNoContent_WhenRoomIsDeleted()
+    {
+        _mockRoomService.Setup(service => service.DeleteRoomAsync(It.IsAny<int>())).ReturnsAsync(true);
+
+        var result = await _controller.DeleteRoom(1);
+
+        result.Should().BeOfType<NoContentResult>();
+    }
+
+    [Fact]
+    public async Task GetDirtyRooms_ShouldReturnOkWithDirtyRooms()
+    {
+        var dirtyRooms = new List<Room>
+        {
+            new Room { Id = 1, RoomNumber = "101", RoomType = "Single", Status = RoomStatus.Dirty }
+        };
+
+        _mockRoomService.Setup(service => service.GetDirtyRoomsAsync()).ReturnsAsync(dirtyRooms);
+
+        var result = await _controller.GetDirtyRooms();
+
+        result.Should().BeOfType<OkObjectResult>();
+        var okResult = result as OkObjectResult;
+        okResult!.Value.Should().BeEquivalentTo(dirtyRooms);
+    }
+}
---
+++ RoomService.Tests/RoomServiceTest.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RoomService.Data;
+using RoomService.Models;
+using RoomService.Services;
+using Xunit;
+using FluentAssertions;
+
+namespace RoomService.Tests
+{
+    public class RoomServiceTests
+    {
+        private readonly RoomDbContext _context;
+        private readonly Services.RoomService _roomService;
+
+        public RoomServiceTests()
+        {
+            var options = new DbContextOptionsBuilder<RoomDbContext>()
+                .UseInMemoryDatabase(databaseName: "RoomServiceTestDb")
+                .Options;
+
+            _context = new RoomDbContext(options);
+            _roomService = new Services.RoomService(_context);
+        }
+        
+        
+        public void Dispose()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
+        }
+        
+        [Fact]
+        public async Task AddRoomAsync_ShouldAddRoomSuccessfully()
+        {
+            var room = new Room
+            {
+                RoomNumber = "101",
+                RoomType = "Single",
+                Capacity = 1,
+                Status = RoomStatus.Clean,
+                Price = 100.0m
+            };
+
+            var addedRoom = await _roomService.AddRoomAsync(room);
+
+            var dbRoom = await _context.Rooms.FirstOrDefaultAsync(r => r.Id == addedRoom.Id);
+            dbRoom.Should().NotBeNull();
+            dbRoom!.RoomNumber.Should().Be("101");
+            dbRoom.Status.Should().Be(RoomStatus.Clean);
+            Dispose();
+        }
+
+        [Fact]
+        public async Task GetAllRoomsAsync_ShouldReturnAllRooms()
+        {
+            _context.Rooms.Add(new Room { RoomNumber = "101", RoomType = "Single", Capacity = 1, Status = RoomStatus.Clean, Price = 100.0m });
+            _context.Rooms.Add(new Room { RoomNumber = "102", RoomType = "Double", Capacity = 2, Status = RoomStatus.Dirty, Price = 200.0m });
+            await _context.SaveChangesAsync();
+
+            var rooms = await _roomService.GetAllRoomsAsync();
+
+            rooms.Should().HaveCount(2);
+            Dispose();
+        }
+
+        [Fact]
+        public async Task UpdateRoomAsync_ShouldUpdateRoomSuccessfully()
+        {
+            var room = new Room
+            {
+                RoomNumber = "101",
+                RoomType = "Single",
+                Capacity = 1,
+                Status = RoomStatus.Clean,
+                Price = 100.0m
+            };
+
+            _context.Rooms.Add(room);
+            await _context.SaveChangesAsync();
+
+            var updatedRoom = new Room
+            {
+                Id = room.Id,
+                RoomNumber = "101A",
+                RoomType = "Deluxe",
+                Capacity = 2,
+                Status = RoomStatus.Dirty,
+                Price = 150.0m
+            };
+
+            var result = await _roomService.UpdateRoomAsync(room.Id, updatedRoom);
+
+            result.Should().NotBeNull();
+            result!.RoomType.Should().Be("Deluxe");
+            result.Status.Should().Be(RoomStatus.Dirty);
+            result.Price.Should().Be(150.0m);
+            Dispose();
+        }
+
+        [Fact]
+        public async Task DeleteRoomAsync_ShouldDeleteRoomSuccessfully()
+        {
+            var room = new Room
+            {
+                RoomNumber = "101",
+                RoomType = "Single",
+                Capacity = 1,
+                Status = RoomStatus.Clean,
+                Price = 100.0m
+            };
+
+            _context.Rooms.Add(room);
+            await _context.SaveChangesAsync();
+
+            var result = await _roomService.DeleteRoomAsync(room.Id);
+
+            result.Should().BeTrue();
+            var dbRoom = await _context.Rooms.FindAsync(room.Id);
+            dbRoom.Should().BeNull();
+            Dispose();
+        }
+
+        [Fact]
+        public async Task GetRoomByRoomNumberAsync_ShouldReturnCorrectRoom()
+        {
+            var room = new Room
+            {
+                RoomNumber = "101",
+                RoomType = "Single",
+                Capacity = 1,
+                Status = RoomStatus.Clean,
+                Price = 100.0m
+            };
+
+            _context.Rooms.Add(room);
+            await _context.SaveChangesAsync();
+
+            var result = await _roomService.GetRoomByRoomNumberAsync("101");
+
+            result.Should().NotBeNull();
+            result!.RoomType.Should().Be("Single");
+            result.Status.Should().Be(RoomStatus.Clean);
+            Dispose();
+        }
+
+        [Fact]
+        public async Task GetDirtyRoomsAsync_ShouldReturnOnlyDirtyRooms()
+        {
+            _context.Rooms.Add(new Room { RoomNumber = "101", RoomType = "Single", Capacity = 1, Status = RoomStatus.Dirty, Price = 100.0m });
+            _context.Rooms.Add(new Room { RoomNumber = "102", RoomType = "Double", Capacity = 2, Status = RoomStatus.Clean, Price = 200.0m });
+            await _context.SaveChangesAsync();
+
+            var dirtyRooms = await _roomService.GetDirtyRoomsAsync();
+
+            dirtyRooms.Should().HaveCount(1);
+            dirtyRooms.First().RoomNumber.Should().Be("101");
+            Dispose();
+        }
+    }
+}
